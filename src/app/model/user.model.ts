ce6afa52925e8cfa59e9825d086d8da50f511a22
SamarakoonRMSK <rmsksamarakoon@gmail.com>
2025-07-18 17:35:39 +0530

    Implement login functionality with user service and update UI components

---
+++ src/app/model/user.model.ts
@@ -0,0 +1,23 @@
+export class User{
+
+  emailId: string;
+  password: string;
+
+  constructor(){
+    this.emailId = '';
+    this.password = '';
+  }
+}
+
+export interface IUserModel{
+
+  userId: number;
+  emailId: string;
+  password: string;
+  createdDate: string;
+  projectName: string;
+  fullName: string;
+  mobileNo: string;
+  extraId: number;
+
+}
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject, NgModule } from '@angular/core';
+import { IUserModel, User } from '../../model/user.model';
+import { UserService } from '../../services/user.service';
+import {FormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  loginObj:User = new User();
+
+  userSrv = inject(UserService);
+
+
+  onLogin(){
+    this.userSrv.loginUser(this.loginObj).subscribe({
+      next:(res:IUserModel)=>{
+        alert("User logged");
+      },
+      error:(error)=>{
+        alert("User login failed");
+      }
+    })
+  }
 
 }
