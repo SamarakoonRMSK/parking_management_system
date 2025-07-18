c3d6d14c6c4476991d0000afcfb8f2bdc18f7784
SamarakoonRMSK <rmsksamarakoon@gmail.com>
2025-07-18 21:37:46 +0530

    feat: Add dashboard layout ui

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, inject, NgModule } from '@angular/core';
 import { IUserModel, User } from '../../model/user.model';
 import { UserService } from '../../services/user.service';
 import {FormsModule} from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +13,7 @@ import {FormsModule} from '@angular/forms';
 export class LoginComponent {
 
   loginObj:User = new User();
-
+  router = inject(Router);
   userSrv = inject(UserService);
 
 
@@ -20,6 +21,8 @@ export class LoginComponent {
     this.userSrv.loginUser(this.loginObj).subscribe({
       next:(res:IUserModel)=>{
         alert("User logged");
+        localStorage.setItem("user",JSON.stringify(res));
+        this.router.navigateByUrl("/dashboard");
       },
       error:(error)=>{
         alert("User login failed");
